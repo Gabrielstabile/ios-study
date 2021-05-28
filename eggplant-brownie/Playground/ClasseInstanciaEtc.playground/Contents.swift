c067e1849303bf579649ba97c2d4e3ea03f5b18e
Gabriel Stabile <gabriel.stabile@neon.com.br>
2021-05-28 10:19:45 -0300

    uploading new playground: class

---
+++ eggplant-brownie/Playground/ClasseInstanciaEtc.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// Exemplo de criacao de classe com Swift
+
+class Refeicao {
+    var nomeDaComida: String?
+    var felicidade: String?
+}
+
+// duas formas de como estanciar uma classe: forma implicita e explicita
+
+/* let refeicao = Refeicao */
+let refeicao: Refeicao = Refeicao()
+
+refeicao.nomeDaComida = "churrasco"
+
+/* usar o " ! " para extrair o valor de uma variável chama-se FORCED UNWRAP, devemos usar com cuidado
+   pois caso a variavel que estamos tentando extrair o valor esteja nula, vai causar um fatal error
+   e gerar um crash em nosso aplicativo
+ */
+
+print(refeicao.nomeDaComida!)
