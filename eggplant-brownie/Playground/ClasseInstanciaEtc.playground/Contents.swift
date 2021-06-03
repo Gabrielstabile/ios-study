792bb65918c1bfc46be9f0768e6ff95d01dce736
Gabriel Stabile <gabriel.stabile@neon.com.br>
2021-06-03 15:04:53 -0300

    Adding if let and guard let

--- eggplant-brownie/Playground/ClasseInstanciaEtc.playground/Contents.swift
+++ eggplant-brownie/Playground/ClasseInstanciaEtc.playground/Contents.swift
@@ -19,4 +19,28 @@ refeicao.nomeDaComida = "churrasco"
    e gerar um crash em nosso aplicativo
  */
 
-print(refeicao.nomeDaComida!)
+/* print(refeicao.nomeDaComida!) */
+
+if (refeicao.nomeDaComida != nil){
+//    print(refeicao.nomeDaComida!)
+}
+
+// Boas práticas para extrair valores opcionais:
+
+// com o if let, conseguimos tratar o uso de variáveis opcionais, é recomendado utilizar if let ou o guard let logo abaixo. não gerá um crash caso o valor seja nulo.
+if let nomeDaComida = refeicao.nomeDaComida{
+    print(nomeDaComida)
+}
+
+    // guard let: usando o guard let, eu consigo criar uma constante e verificar se o valor dela não é nulo, é a forma mais recomendada para se tratar variáveis opcionais, caso caia no return a aplicação irá gerar um crash (caso o valor seja nulo).
+
+func exibeNomeDaRefeicao() {
+    guard let nomeDaComida = refeicao.nomeDaComida else {
+        return
+    }
+    
+    print(nomeDaComida)
+}
+
+exibeNomeDaRefeicao()
+
